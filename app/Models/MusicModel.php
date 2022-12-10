d5606c2941dd894d1a4e729e933658be1806b40f
Ortiv Inga <ortiv@programmer.net>
2022-12-10 10:46:46 -0500

    task: added modal fields; to-do: make fields required, add form helper

--- app/Models/MusicModel.php
+++ app/Models/MusicModel.php
@@ -47,33 +47,117 @@ class MusicModel extends Model
 
   public function musicModal()
   {
-    $modal = '<div>
-              <!-- Button trigger modal -->
-              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-              Add Music
-              </button>
+    $modal = '<div class="container">
+                  <h2>Basic Modal Example</h2>
+                  <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  Launch demo modal
+                  </button>
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                  <div class="modal-content">
+                  <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Add song</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
 
-              <!-- Modal -->
-              <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-              <div class="modal-dialog modal-dialog-centered" role="document">
-              <div class="modal-content">
-              <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-              </button>
-              </div>
-              <div class="modal-body">
-              ...
-              </div>
-              <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-              </div>
-              </div>
-              </div>
-              </div>
-              </div>';
+                  <!-- Start form -->
+                  
+                  <div class="input-group mb-3">
+                  <label class="input-group-text"
+                  for="inputGroupSelect01">Artist</label>
+                  <input class="form-control form-control-sm"
+                  type="text" placeholder="ex: Boyz II Men"
+                  aria-label=".form-control-sm example">
+                  </div>
+
+                  <div class="input-group mb-3">
+                  <label class="input-group-text"
+                  for="inputGroupSelect01">Title</label>
+                  <input class="form-control form-control-sm"
+                  type="text" aria-label=".form-control-sm example">
+                  </div>                  
+
+                  <div class="input-group mb-3">
+                  <label class="input-group-text"
+                  for="inputGroupSelect01">Format</label>
+                  <select class="form-select"
+                  id="inputGroupSelect01">
+                  <option selected>Choose...</option>
+                  <option value="ELECTRONIC">Electronic</option>
+                  <option value="PHYSICAL">Physical</option>
+                  </select>
+                  </div>
+
+                  <div class="input-group mb-3">
+                  <label class="input-group-text"
+                  for="inputGroupSelect01">Album</label>
+                  <input class="form-control form-control-sm"
+                  type="text" aria-label=".form-control-sm example">
+                  </div>     
+                  
+                  <div class="input-group mb-3">
+                  <label class="input-group-text"
+                  for="inputGroupSelect01">Length</label>
+                  <input class="text-sm-start" id="duration-input" type="text" required pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}" value="00:00:00" title="Write a duration in the format hh:mm:ss class="form-control form-control-sm"
+                  type="text" placeholder="5" aria-label=".form-control-sm example" style="
+                  font-size: small;">
+                  <p style="
+                  font-size: small;" id="output"></p>
+                  </div> 
+
+                  <!-- time format script -->
+                  <script>
+                  {let durationIn = document.getElementById("duration-input");
+                    let resultP = document.getElementById("output");
+                    durationIn.addEventListener("change", function (e) {
+                    resultP.textContent = "";
+                    durationIn.checkValidity();
+                    });
+                    durationIn.addEventListener("invalid", function (e) {
+                  resultP.textContent = "Invalid format: must be minute:seconds:milliseconds";
+                });
+                }
+                </script> 
+                <!-- time format script -->
+
+                <div class="input-group mb-3">
+                <label class="input-group-text"
+                for="inputGroupSelect01">Genre</label>
+                <input class="form-control form-control-sm"
+                type="text" aria-label=".form-control-sm example">
+                </div>   
+
+                <div class="input-group mb-3">
+                <label class="input-group-text"
+                for="inputGroupSelect01">Vendor</label>
+                <input class="form-control form-control-sm"
+                type="text" aria-label=".form-control-sm example">
+                </div> 
+
+                <div class="input-group mb-3">
+                <label class="input-group-text"
+                for="inputGroupSelect01">Property Type</label>
+                <select class="form-select"
+                id="inputGroupSelect01">
+                <option selected>Choose...</option>
+                <option value="PURCHASE">Purchase</option>
+                <option value="GIFT">Gift</option>
+                </select>
+                </div>                
+
+                  <! -- End form -->
+                  </div>
+                  <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>';
     return $modal;
   }
 
--- app/Views/common/tail.php
+++ app/Views/common/tail.php
@@ -3,6 +3,8 @@
     <p class="navbar-text pull-left">© 2014 - Content #0
       <a href="http://tinyurl.com/tbvalid" target="_blank">Content #1</a>
     </p>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 
     <a href="" class="navbar-btn btn-danger btn pull-right">
       <span class="glyphicon glyphicon-star"></span>Button ref</a>
